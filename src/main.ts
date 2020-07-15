062e9f0d5d2009acb867facb01eb8076289b34be
Gitonga Kithinji <gitonga.kithinji@thepalladiumgroup.com>
2020-07-15 16:09:54 +0300

    ensure that https is active only on production
    Signed-off-by: Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,17 @@ import * as cookieParser from 'cookie-parser';
 import * as fs from 'fs';
 
 async function bootstrap() {
-    const httpsOptions = {
-        key: fs.readFileSync('./secrets/kenyahmis.org.key'),
-        cert: fs.readFileSync('./secrets/kenyahmis.org.crt'),
-    };
+    const keyFile = fs.readFileSync('./secrets/kenyahmis.org.key');
+    const certFile = fs.readFileSync('./secrets/kenyahmis.org.crt');
+    const options = process.env.NODE_ENV.trim() === 'production' ? {
+        key: keyFile,
+        cert: certFile,
+    } : null;
 
     const app = await NestFactory.create(AppModule, {
-        httpsOptions
+        httpsOptions: options
     });
+
     app.setGlobalPrefix('api');
     app.enableCors();
     app.use(helmet());
