d600f9de60875dabf878daae63226b9686f3fe9b
Eric Ejimba <ejimba@gmail.com>
2020-08-24 13:23:11 +0300

    increase rate limits for all API endpoints

--- src/main.ts
+++ src/main.ts
@@ -23,8 +23,8 @@ async function bootstrap() {
     app.use(csurf({ cookie: true }));
     app.use(
         rateLimit({
-            windowMs: 15 * 60 * 1000, // 15 minutes
-            max: 1000, // limit each IP to 100 requests per windowMs
+            windowMs: 2 * 60 * 1000, // 2 minutes
+            max: 10000, // limit each IP to 10000 requests per windowMs
         }),
     );
     await app.listen(process.env.APP_PORT || 4000);
