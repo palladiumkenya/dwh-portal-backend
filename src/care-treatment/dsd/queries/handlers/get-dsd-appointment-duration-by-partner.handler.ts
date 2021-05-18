7d08fbe70e91a3de4f3cbf78fb31c9194d8b9cd6
felix kithinji <felixkithinji@outlook.com>
2021-05-18 18:45:11 +0300

    bug fixes - DISTRIBUTION OF STABLE PATIENTS BY AGE GROUP AND SEX - DISTRIBUTION OF STABLE PATIENTS BY COUNTY - DISTRIBUTION OF STABLE PATIENTS BY COUNTY

--- src/care-treatment/dsd/queries/handlers/get-dsd-appointment-duration-by-partner.handler.ts
+++ src/care-treatment/dsd/queries/handlers/get-dsd-appointment-duration-by-partner.handler.ts
@@ -15,7 +15,7 @@ export class GetDsdAppointmentDurationByPartnerHandler implements IQueryHandler<
 
     async execute(query: GetDsdAppointmentDurationByPartnerQuery): Promise<any> {
         const dsdAppointmentDuration = this.repository.createQueryBuilder('f')
-            .select(['SUM(TXCurr) patients, SUM([StabilityAssessment]) stablePatients, CTPartner partner, (CAST(SUM([StabilityAssessment]) as float)/CAST(SUM(TXCurr) as float)) percentStable'])
+            .select(['SUM(TXCurr) patients, DATIM_AgeGroup ageGroup, SUM([StabilityAssessment]) stablePatients, CTPartner partner, (CAST(SUM([StabilityAssessment]) as float)/CAST(SUM(TXCurr) as float)) percentStable'])
             .where('f.MFLCode > 1')
             .andWhere('f.Stability = :stability', { stability: "Stable"});
 
@@ -36,7 +36,7 @@ export class GetDsdAppointmentDurationByPartnerHandler implements IQueryHandler<
         }
 
         return await dsdAppointmentDuration
-            .groupBy('CTPartner')
+            .groupBy('CTPartner, DATIM_AgeGroup')
             .orderBy('percentStable', 'DESC')
             .getRawMany();
     }
