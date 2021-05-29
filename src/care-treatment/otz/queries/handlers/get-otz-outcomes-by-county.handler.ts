a0d76661a3f447756242bd8b8a4abefa4b62c608
felix kithinji <felixkithinji@outlook.com>
2021-05-29 12:17:22 +0300

    - OTZ Outcomes

---
+++ src/care-treatment/otz/queries/handlers/get-otz-outcomes-by-county.handler.ts
@@ -0,0 +1,40 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { GetOtzOutcomesByCountyQuery } from '../impl/get-otz-outcomes-by-county.query';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FactTransOtzOutcome } from '../../entities/fact-trans-otz-outcome.model';
+import { Repository } from 'typeorm';
+
+@QueryHandler(GetOtzOutcomesByCountyQuery)
+export class GetOtzOutcomesByCountyHandler implements IQueryHandler<GetOtzOutcomesByCountyQuery> {
+    constructor(
+        @InjectRepository(FactTransOtzOutcome, 'mssql')
+        private readonly repository: Repository<FactTransOtzOutcome>
+    ) {
+    }
+
+    async execute(query: GetOtzOutcomesByCountyQuery): Promise<any> {
+        const otzOutcomesByCounty = this.repository.createQueryBuilder('f')
+            .select(['[County], [Outcome], SUM([Total_OutCome]) outcomesByCounty'])
+            .andWhere('f.MFLCode IS NOT NULL');
+
+        if (query.county) {
+            otzOutcomesByCounty.andWhere('f.County IN (:...counties)', { counties: query.county });
+        }
+
+        if (query.subCounty) {
+            otzOutcomesByCounty.andWhere('f.SubCounty IN (:...subCounties)', { subCounties: query.subCounty });
+        }
+
+        if (query.facility) {
+            otzOutcomesByCounty.andWhere('f.FacilityName IN (:...facilities)', { facilities: query.facility });
+        }
+
+        if (query.partner) {
+            otzOutcomesByCounty.andWhere('f.CTPartner IN (:...partners)', { partners: query.partner });
+        }
+
+        return await otzOutcomesByCounty
+            .groupBy('[County], [Outcome]')
+            .getRawMany();
+    }
+}
