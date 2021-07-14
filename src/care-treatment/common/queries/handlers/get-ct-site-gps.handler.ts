58c3017f90891585dec6b1067e378af8162245b0
Felix Kithinji Gitonga <kithinjifelix@gmail.com>
2021-07-14 14:02:11 +0300

    remove IRDO sites from reporting rates

--- src/care-treatment/common/queries/handlers/get-ct-site-gps.handler.ts
+++ src/care-treatment/common/queries/handlers/get-ct-site-gps.handler.ts
@@ -18,6 +18,7 @@ export class GetCtSiteGpsHandler implements IQueryHandler<GetCtSiteGpsQuery> {
             .select(['facilityId mfl, name facility, county, subCounty, agency, partner, owner, latitude, longitude, LTRIM(RTRIM(EMR)) emr, isCT, isPkv, isHts'])
             .where('q.name IS NOT NULL')
             .andWhere('q.EMR IS NOT NULL')
+            .andWhere("q.partner <> 'IRDO'")
             .andWhere("q.EMR <> ''");
 
         if (query.county) {
