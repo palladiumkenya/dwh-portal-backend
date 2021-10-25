30e28e8d82f2cdaa0e2c0c9231b12ca27c423c10
Felix Kithinji Gitonga <kithinjifelix@gmail.com>
2021-10-25 12:54:29 +0300

    - PERCENTAGE OF PLHIV WHO MISSED APPOINTMENT DUE TO COVID -19 INFECTION BY AGE - PERCENTAGE OF PLHIV WHO MISSED APPOINTMENTS DUE TO COVID-19 INFECTION BY COUNTY - PERCENTAGE OF PLHIV WHO MISSED APPOINTMENTS DUE TO COVID-19 INFECTION BY PARTNER

---
+++ src/care-treatment/covid/queries/handlers/get-covid-percentage-who-missed-appointments-by-county.handler.ts
@@ -0,0 +1,44 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { GetCovidPercentageWhoMissedAppointmentsByCountyQuery } from '../impl/get-covid-percentage-who-missed-appointments-by-county.query';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FactTransCovidVaccines } from '../../entities/fact-trans-covid-vaccines.model';
+import { Repository } from 'typeorm';
+import { FactTransNewCohort } from '../../../new-on-art/entities/fact-trans-new-cohort.model';
+import { DimAgeGroups } from '../../../common/entities/dim-age-groups.model';
+
+@QueryHandler(GetCovidPercentageWhoMissedAppointmentsByCountyQuery)
+export class GetCovidPercentageWhoMissedAppointmentsByCountyHandler implements IQueryHandler<GetCovidPercentageWhoMissedAppointmentsByCountyQuery> {
+    constructor(
+        @InjectRepository(FactTransCovidVaccines, 'mssql')
+        private readonly repository: Repository<FactTransCovidVaccines>
+    ) {
+    }
+
+    async execute(query: GetCovidPercentageWhoMissedAppointmentsByCountyQuery): Promise<any> {
+        const covidPercentageWhoMissedAppointmentsByCounty = this.repository.createQueryBuilder('f')
+            .select(['County, count (*)Num'])
+            .leftJoin(FactTransNewCohort, 'g', 'f.PatientID = g.PatientID and f.SiteCode = g.MFLCode and f.PatientPK = g.PatientPK')
+            .innerJoin(DimAgeGroups, 'v', 'g.ageLV = v.Age')
+            .where('MissedAppointmentDueToCOVID19=\'Yes\' AND County IS NOT NULL');
+
+        if (query.county) {
+            covidPercentageWhoMissedAppointmentsByCounty.andWhere('f.County IN (:...counties)', { counties: query.county });
+        }
+
+        if (query.subCounty) {
+            covidPercentageWhoMissedAppointmentsByCounty.andWhere('f.SubCounty IN (:...subCounties)', { subCounties: query.subCounty });
+        }
+
+        if (query.facility) {
+            covidPercentageWhoMissedAppointmentsByCounty.andWhere('f.FacilityName IN (:...facilities)', { facilities: query.facility });
+        }
+
+        if (query.partner) {
+            covidPercentageWhoMissedAppointmentsByCounty.andWhere('f.CTPartner IN (:...partners)', { partners: query.partner });
+        }
+
+        return await covidPercentageWhoMissedAppointmentsByCounty
+            .groupBy('County')
+            .getRawMany();
+    }
+}
