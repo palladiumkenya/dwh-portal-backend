fbe377091d0e1a0fc79ffd570847165597b91210
Felix Kithinji Gitonga <kithinjifelix@gmail.com>
2021-11-03 10:52:46 +0300

    art optimization - adults

--- src/care-treatment/art-optimization/queries/handlers/get-art-optimization-current-by-age-sex.handler.ts
+++ src/care-treatment/art-optimization/queries/handlers/get-art-optimization-current-by-age-sex.handler.ts
@@ -34,9 +34,9 @@ export class GetArtOptimizationCurrentByAgeSexHandler implements IQueryHandler<G
             artOptimizationCurrentByAgeSex.andWhere('f.CTPartner IN (:...partner)', { partner: query.partner });
         }
 
-        // if (query.agency) {
-        //     artOptimizationCurrentByAgeSex.andWhere('f.agency IN (:...agency)', { agency: query.agency });
-        // }
+        if (query.agency) {
+            artOptimizationCurrentByAgeSex.andWhere('f.CTAgency IN (:...agency)', { agency: query.agency });
+        }
 
         // if (query.project) {
         //     artOptimizationCurrentByAgeSex.andWhere('f.project IN (:...project)', { project: query.project });
