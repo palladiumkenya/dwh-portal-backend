c9d3e8c37b20a137a0f215911f61925ebd341e17
cbrianbet <cbrianbet@outlook.com>
2022-03-29 17:07:14 +0300

    Update get-linkage-number-positive.handler.ts

--- src/hts/linkage/queries/handlers/get-linkage-number-positive.handler.ts
+++ src/hts/linkage/queries/handlers/get-linkage-number-positive.handler.ts
@@ -1,8 +1,8 @@
-import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
-import { GetLinkageNumberPositiveQuery } from '../impl/get-linkage-number-positive.query';
-import { InjectRepository } from '@nestjs/typeorm';
-import { FactHtsUptake } from '../../entities/fact-htsuptake.entity';
-import { Repository } from 'typeorm';
+import {IQueryHandler, QueryHandler} from '@nestjs/cqrs';
+import {GetLinkageNumberPositiveQuery} from '../impl/get-linkage-number-positive.query';
+import {InjectRepository} from '@nestjs/typeorm';
+import {FactHtsUptake} from '../../entities/fact-htsuptake.entity';
+import {Repository} from 'typeorm';
 
 
 @QueryHandler(GetLinkageNumberPositiveQuery)
@@ -21,42 +21,40 @@ export class GetLinkageNumberPositiveHandler implements IQueryHandler<GetLinkage
             '((SUM(CASE WHEN linked IS NULL THEN 0 ELSE linked END)/SUM(positive))*100) AS linkage ' +
             'FROM fact_htsuptake WHERE positive > 0 ';
 
-        if(query.county) {
+        if (query.county) {
             linkageNumberPositiveSql = `${linkageNumberPositiveSql} and County IN (?)`;
             params.push(query.county);
         }
 
-        if(query.subCounty) {
+        if (query.subCounty) {
             linkageNumberPositiveSql = `${linkageNumberPositiveSql} and SubCounty IN (?)`;
             params.push(query.subCounty);
         }
-        
-        if(query.facility) {
+
+        if (query.facility) {
             linkageNumberPositiveSql = `${linkageNumberPositiveSql} and FacilityName IN (?)`;
             params.push(query.facility);
         }
 
-        if(query.partner) {
+        if (query.partner) {
             linkageNumberPositiveSql = `${linkageNumberPositiveSql} and CTPartner IN (?)`;
             params.push(query.partner);
         }
 
-        if(query.year) {
-            if(query.year == (new Date()).getFullYear()) {
-                linkageNumberPositiveSql = `${linkageNumberPositiveSql} and  (YEAR >= YEAR(DATE_SUB(NOW(), INTERVAL 11 MONTH)))`;
-            } else {
-                linkageNumberPositiveSql = `${linkageNumberPositiveSql} and year=?`;
-            }
+        if (query.year) {
+
+            linkageNumberPositiveSql = `${linkageNumberPositiveSql} and year=?`;
             params.push(query.year);
+
         }
 
-        if(query.month) {
+        if (query.month) {
             linkageNumberPositiveSql = `${linkageNumberPositiveSql} and month=?`;
             params.push(query.month);
         }
 
         linkageNumberPositiveSql = `${linkageNumberPositiveSql} GROUP BY year, month ORDER BY year, month`;
 
-        return  await this.repository.query(linkageNumberPositiveSql, params);
+        return await this.repository.query(linkageNumberPositiveSql, params);
     }
 }
