92909c28ab5da5a97e91c7868a37d1d252cd718c
cbrianbet <cbrianbet@outlook.com>
2022-04-13 16:46:53 +0300

    improved query for filters

--- src/care-treatment/viral-load/queries/handlers/get-vl-overall-uptake-and-suppression-ldl.handler.ts
+++ src/care-treatment/viral-load/queries/handlers/get-vl-overall-uptake-and-suppression-ldl.handler.ts
@@ -16,41 +16,50 @@ export class GetVlOverallUptakeAndSuppressionLdlHandler implements IQueryHandler
     }
 
     async execute(query: GetVlOverallUptakeAndSuppressionLdlQuery): Promise<any> {
-        // const vlOverallUptakeAndSuppressionBySex = this.repository.createQueryBuilder('f')
-        //     .select(['Last12MVLResult, Gender gender, COUNT ( * ) Num'])
-        //     .where('f.MFLCode > 0')
-        //     .andWhere('Last12MVLResult IS NOT NULL');
-        console.log(query)
+        const vlOverallUptakeAndSuppressinLDL = this.repository.createQueryBuilder('f')
+            .select(['LastVL, lastVLDate, CASE WHEN ISNUMERIC(LastVL)=1 THEN CASE WHEN CAST(Replace(LastVL,\',\',\'\')AS FLOAT) <=50.90 THEN \'<50 Copies\' WHEN CAST(Replace(LastVL,\',\',\'\') AS FLOAT) between 51.00 and 399.00 THEN \'51-399\' WHEN CAST(Replace(LastVL,\',\',\'\')AS FLOAT) between 400.00 and 999.00 THEN \'400-999\' WHEN CAST(Replace(LastVL,\',\',\'\')AS FLOAT) >=1000 THEN \'>1000 Copies\' END WHEN LastVL IN (\'undetectable\',\'NOT DETECTED\',\'0 copies/ml\',\'LDL\',\'ND\',\'Target Not Detected\',\' Not detected\',\'Target Not Detected.\',\'Less than Low Detectable Level\') THEN \'<50 Copies\'  ELSE NULL END AS [Last12MVLResult]'])
+            .where('ARTOutcome=\'V\' and DATEDIFF(MONTH,lastVLDate,GETDATE())<= 14')
 
-        const vlOverallUptakeAndSuppressinLDL = "With VL AS (SELECT LastVL, lastVLDate, CASE  WHEN ISNUMERIC(LastVL)=1  THEN CASE  WHEN CAST(Replace(LastVL,',','')AS FLOAT) <=50.90 THEN '<50 Copies' WHEN CAST(Replace(LastVL,',','') AS FLOAT) between 51.00 and 399.00 THEN '51-399' WHEN CAST(Replace(LastVL,',','')AS FLOAT) between 400.00 and 999.00 THEN '400-999' WHEN CAST(Replace(LastVL,',','')AS FLOAT) >=1000 THEN '>1000 Copies' END WHEN LastVL IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<50 Copies' ELSE NULL END AS [Last12MVLResult] FROM PortalDev.dbo.Fact_Trans_New_Cohort where ARTOutcome='V' and DATEDIFF(MONTH,lastVLDate,GETDATE())<= 14 )  SELECT Last12MVLResult, Count(*) Num FROM VL where Last12MVLResult in ('<50 Copies','400-999','51-399') Group by Last12MVLResult"
+        // const vlOverallUptakeAndSuppressinLDL = "With VL AS (SELECT LastVL, lastVLDate, CASE  WHEN ISNUMERIC(LastVL)=1  THEN CASE  WHEN CAST(Replace(LastVL,',','')AS FLOAT) <=50.90 THEN '<50 Copies' WHEN CAST(Replace(LastVL,',','') AS FLOAT) between 51.00 and 399.00 THEN '51-399' WHEN CAST(Replace(LastVL,',','')AS FLOAT) between 400.00 and 999.00 THEN '400-999' WHEN CAST(Replace(LastVL,',','')AS FLOAT) >=1000 THEN '>1000 Copies' END WHEN LastVL IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<50 Copies' ELSE NULL END AS [Last12MVLResult] FROM PortalDev.dbo.Fact_Trans_New_Cohort where ARTOutcome='V' and DATEDIFF(MONTH,lastVLDate,GETDATE())<= 14 )  SELECT Last12MVLResult, Count(*) Num FROM VL where Last12MVLResult in ('<50 Copies','400-999','51-399') Group by Last12MVLResult"
 
-        // if (query.county) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.County IN (:...counties)', {counties: query.county});
-        // }
-        //
-        // if (query.subCounty) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.SubCounty IN (:...subCounties)', {subCounties: query.subCounty});
-        // }
-        //
-        // if (query.facility) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.FacilityName IN (:...facilities)', {facilities: query.facility});
-        // }
-        //
-        // if (query.partner) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.CTPartner IN (:...partners)', {partners: query.partner});
-        // }
-        //
-        // if (query.agency) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.CTAgency IN (:...agencies)', {agencies: query.agency});
-        // }
-        //
-        // if (query.datimAgeGroup) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.AgeGroup IN (:...ageGroups)', {ageGroups: query.datimAgeGroup});
-        // }
-        //
-        // if (query.gender) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.Gender IN (:...genders)', {genders: query.gender});
-        // }
-        return await this.repository.query(vlOverallUptakeAndSuppressinLDL);
+        if (query.county) {
+            vlOverallUptakeAndSuppressinLDL.andWhere('f.County IN (:...counties)', {counties: query.county});
+        }
+
+        if (query.subCounty) {
+            vlOverallUptakeAndSuppressinLDL.andWhere('f.SubCounty IN (:...subCounties)', {subCounties: query.subCounty});
+        }
+
+        if (query.facility) {
+            vlOverallUptakeAndSuppressinLDL.andWhere('f.FacilityName IN (:...facilities)', {facilities: query.facility});
+        }
+
+        if (query.partner) {
+            vlOverallUptakeAndSuppressinLDL.andWhere('f.CTPartner IN (:...partners)', {partners: query.partner});
+        }
+
+        if (query.agency) {
+            vlOverallUptakeAndSuppressinLDL.andWhere('f.CTAgency IN (:...agencies)', {agencies: query.agency});
+        }
+
+        if (query.datimAgeGroup) {
+            vlOverallUptakeAndSuppressinLDL.andWhere('f.AgeGroup IN (:...ageGroups)', {ageGroups: query.datimAgeGroup});
+        }
+
+        if (query.gender) {
+            vlOverallUptakeAndSuppressinLDL.andWhere('f.Gender IN (:...genders)', {genders: query.gender});
+        }
+
+        const originalQuery = vlOverallUptakeAndSuppressinLDL.getQuery
+        const originalParams = vlOverallUptakeAndSuppressinLDL.getParameters
+        vlOverallUptakeAndSuppressinLDL.getQuery = () => {
+            const a = originalQuery.call(vlOverallUptakeAndSuppressinLDL)
+            return `WITH VL AS (${a}) SELECT Last12MVLResult, Count(*) Num FROM VL WHERE Last12MVLResult in ('<50 Copies','400-999','51-399') Group by Last12MVLResult`
+        }
+        vlOverallUptakeAndSuppressinLDL.getParameters = () => {
+            return originalParams.call(vlOverallUptakeAndSuppressinLDL)
+        }
+
+        return  vlOverallUptakeAndSuppressinLDL.getRawMany();
     }
 }
--- src/care-treatment/viral-load/queries/handlers/get-vl-overall-uptake-and-suppression-refered-less-intense.handler.ts
+++ src/care-treatment/viral-load/queries/handlers/get-vl-overall-uptake-and-suppression-refered-less-intense.handler.ts
@@ -15,42 +15,51 @@ export class GetVlOverallUptakeAndSuppressionReferedLessIntenseHandler implement
     }
 
     async execute(query: GetVlOverallUptakeAndSuppressionReferredLessIntenseQuery): Promise<any> {
-        // const vlOverallUptakeAndSuppressionBySex = this.repository.createQueryBuilder('f')
-        //     .select(['Last12MVLResult, Gender gender, COUNT ( * ) Num'])
-        //     .where('f.MFLCode > 0')
-        //     .andWhere('Last12MVLResult IS NOT NULL');
-        console.log(query)
+        const vlOverallUptakeAndSuppressionLessIntense = this.repository.createQueryBuilder('f')
+            .select([`LastVL, lastVLDate, CASE WHEN ISNUMERIC(LastVL)=1 THEN CASE WHEN CAST(Replace(LastVL,',','')AS FLOAT) <=50.90 THEN '<50 Copies' WHEN CAST(Replace(LastVL,',','') AS FLOAT) between 51.00 and 399.00 THEN '51-399' WHEN CAST(Replace(LastVL,',','')AS FLOAT) between 400.00 and 999.00 THEN '400-999' WHEN CAST(Replace(LastVL,',','')AS FLOAT) >=1000 THEN '>1000 Copies' END WHEN LastVL IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<50 Copies' ELSE NULL END AS [Last12MVLResult], DifferentiatedCare`])
+            .where('ARTOutcome=\'V\' and DATEDIFF(MONTH,lastVLDate,GETDATE())<= 14 and DifferentiatedCare<>\'Not Documented\'')
 
-        const vlOverallUptakeAndSuppressInLessIntense = "With DC AS ( SELECT LastVL, lastVLDate, CASE WHEN ISNUMERIC(LastVL)=1 THEN CASE WHEN CAST(Replace(LastVL,',','')AS FLOAT) <=50.90 THEN '<50 Copies' WHEN CAST(Replace(LastVL,',','') AS FLOAT) between 51.00 and 399.00 THEN '51-399' WHEN CAST(Replace(LastVL,',','')AS FLOAT) between 400.00 and 999.00 THEN '400-999' WHEN CAST(Replace(LastVL,',','')AS FLOAT) >=1000 THEN '>1000 Copies' END WHEN LastVL IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<50 Copies' ELSE NULL END AS [Last12MVLResult], DifferentiatedCare FROM PortalDev.dbo.Fact_Trans_New_Cohort where ARTOutcome='V' and DATEDIFF(MONTH,lastVLDate,GETDATE())<= 14 and DifferentiatedCare<>'Not Documented') SELECT count (DifferentiatedCare) Num FROM DC where Last12MVLResult in ('<50 Copies','400-999','51-399')"
+        // const vlOverallUptakeAndSuppressInLessIntense = "With DC AS ( SELECT LastVL, lastVLDate, CASE WHEN ISNUMERIC(LastVL)=1 THEN CASE WHEN CAST(Replace(LastVL,',','')AS FLOAT) <=50.90 THEN '<50 Copies' WHEN CAST(Replace(LastVL,',','') AS FLOAT) between 51.00 and 399.00 THEN '51-399' WHEN CAST(Replace(LastVL,',','')AS FLOAT) between 400.00 and 999.00 THEN '400-999' WHEN CAST(Replace(LastVL,',','')AS FLOAT) >=1000 THEN '>1000 Copies' END WHEN LastVL IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<50 Copies' ELSE NULL END AS [Last12MVLResult], DifferentiatedCare FROM PortalDev.dbo.Fact_Trans_New_Cohort where ARTOutcome='V' and DATEDIFF(MONTH,lastVLDate,GETDATE())<= 14 and DifferentiatedCare<>'Not Documented') SELECT count (DifferentiatedCare) Num FROM DC where Last12MVLResult in ('<50 Copies','400-999','51-399')"
 
 
-        // if (query.county) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.County IN (:...counties)', {counties: query.county});
-        // }
-        //
-        // if (query.subCounty) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.SubCounty IN (:...subCounties)', {subCounties: query.subCounty});
-        // }
-        //
-        // if (query.facility) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.FacilityName IN (:...facilities)', {facilities: query.facility});
-        // }
-        //
-        // if (query.partner) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.CTPartner IN (:...partners)', {partners: query.partner});
-        // }
-        //
-        // if (query.agency) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.CTAgency IN (:...agencies)', {agencies: query.agency});
-        // }
-        //
-        // if (query.datimAgeGroup) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.AgeGroup IN (:...ageGroups)', {ageGroups: query.datimAgeGroup});
-        // }
-        //
-        // if (query.gender) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.Gender IN (:...genders)', {genders: query.gender});
-        // }
-        return await this.repository.query(vlOverallUptakeAndSuppressInLessIntense);
+        if (query.county) {
+            vlOverallUptakeAndSuppressionLessIntense.andWhere('f.County IN (:...counties)', {counties: query.county});
+        }
+
+        if (query.subCounty) {
+            vlOverallUptakeAndSuppressionLessIntense.andWhere('f.SubCounty IN (:...subCounties)', {subCounties: query.subCounty});
+        }
+
+        if (query.facility) {
+            vlOverallUptakeAndSuppressionLessIntense.andWhere('f.FacilityName IN (:...facilities)', {facilities: query.facility});
+        }
+
+        if (query.partner) {
+            vlOverallUptakeAndSuppressionLessIntense.andWhere('f.CTPartner IN (:...partners)', {partners: query.partner});
+        }
+
+        if (query.agency) {
+            vlOverallUptakeAndSuppressionLessIntense.andWhere('f.CTAgency IN (:...agencies)', {agencies: query.agency});
+        }
+
+        if (query.datimAgeGroup) {
+            vlOverallUptakeAndSuppressionLessIntense.andWhere('f.AgeGroup IN (:...ageGroups)', {ageGroups: query.datimAgeGroup});
+        }
+
+        if (query.gender) {
+            vlOverallUptakeAndSuppressionLessIntense.andWhere('f.Gender IN (:...genders)', {genders: query.gender});
+        }
+
+        const originalQuery = vlOverallUptakeAndSuppressionLessIntense.getQuery
+        const originalParams = vlOverallUptakeAndSuppressionLessIntense.getParameters
+        vlOverallUptakeAndSuppressionLessIntense.getQuery = () => {
+            const a = originalQuery.call(vlOverallUptakeAndSuppressionLessIntense)
+            return `WITH DC AS (${a}) SELECT count (DifferentiatedCare) Num FROM DC WHERE Last12MVLResult in ('<50 Copies','400-999','51-399')`
+        }
+        vlOverallUptakeAndSuppressionLessIntense.getParameters = () => {
+            return originalParams.call(vlOverallUptakeAndSuppressionLessIntense)
+        }
+
+        return  vlOverallUptakeAndSuppressionLessIntense.getRawMany();
     }
 }
