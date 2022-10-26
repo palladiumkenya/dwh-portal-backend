30e3db702c045b25e87e0c1ebe5e47bc457189a1
cbrianbet <cbrianbet@outlook.com>
2022-10-26 11:13:18 +0300

    Updated query for UNSUPPRESSED tab charts

--- src/care-treatment/viral-load/queries/handlers/get-vl-overall-number-with-follow-vl-tests-at-gt1000-copies-second-line-regiment.handler.ts
+++ src/care-treatment/viral-load/queries/handlers/get-vl-overall-number-with-follow-vl-tests-at-gt1000-copies-second-line-regiment.handler.ts
@@ -20,40 +20,100 @@ export class GetVlOverallNumberWithFollowVlTestsAtGt1000CopiesSecondLineRegiment
         //     .where('f.MFLCode > 0')
         //     .andWhere('Last12MVLResult IS NOT NULL');
 
-        const vlOverallNumberFollowSecondlineRegiment = "With VL AS (Select MFLCode, uniqueidno_orig as PatientID, faccounty as County, facSubcounty as SubCounty, DOB, Sex, lastVL1 as LastVL, dtlastVL2, dtlastVL1, ARTOutcome, [currentARTline],\n" +
-            "CASE WHEN ISNUMERIC(lastVL1)=1 THEN CASE WHEN CAST(Replace(lastVL1,',','')AS FLOAT) <=50.90 THEN '<1000 Copies' WHEN CAST(Replace(lastVL1,',','') AS FLOAT) between 51.00 and 399.00 THEN '<1000 Copies' WHEN CAST(Replace(lastVL1,',','')AS FLOAT) between 400.00 and 999.00 THEN '<1000 Copies' WHEN CAST(Replace(lastVL1,',','')AS FLOAT) >=1000 THEN '>1000 Copies' END\n" +
-            "WHEN lastVL1 IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<1000 Copies' ELSE NULL END AS [LastVLResult], dtlastVL1 as DateLAstVL, lastVl2,\n" +
-            "CASE WHEN ISNUMERIC(lastVL2)=1 THEN CASE WHEN CAST(Replace(lastVL2,',','')AS FLOAT) <=50.90 THEN '<1000 Copies' WHEN CAST(Replace(lastVL2,',','') AS FLOAT) between 51.00 and 399.00 THEN '<1000 Copies' WHEN CAST(Replace(lastVL2,',','')AS FLOAT) between 400.00 and 999.00 THEN '<1000 Copies' WHEN CAST(Replace(lastVL2,',','')AS FLOAT) >=1000 THEN '>1000 Copies' END WHEN lastVL2 IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<1000 Copies' ELSE NULL END AS [LastVL2Result]\n" +
-            "from All_Staging_2016_2.dbo.Cohort2015_2016 c)\n" +
-            "Select  LastVL2Result, Count (*) Num from VL where ARTOutcome='V' and  LastVL2Result   in ('>1000 Copies')  and DATEDIFF(MONTH,dtlastVL2,GETDATE())<= 14 and currentARTline='Second Line' group by [LastVL2Result]"
+        const vlOverallNumberFollowSecondlineRegiment = this.repository
+            .createQueryBuilder('cohort')
+            .select([
+                `MFLCode,cohort.PatientID,County,SubCounty,DOB,Gender,VL1 as LastVL,VL2Date,VL1Date,ARTOutcome,CurrentRegimenLine As CurrentARTLine,
+                CASE
+                    WHEN ISNUMERIC(VL1)=1
+                    THEN
+                    CASE
+                    WHEN CAST(Replace(VL1,',','')AS FLOAT) <=50.90 THEN '<1000 Copies'
+                    WHEN CAST(Replace(VL1,',','') AS FLOAT) between 51.00 and 399.00 THEN '<1000 Copies'
+                    WHEN CAST(Replace(VL1,',','')AS FLOAT) between 400.00 and 999.00 THEN '<1000 Copies'
+                    WHEN CAST(Replace(VL1,',','')AS FLOAT) >=1000 THEN '>1000 Copies'
+                    END
+                    WHEN VL1 IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<1000 Copies'
+                    ELSE NULL END AS [LastVLResult],
+                        VL1Date as DateLAstVL,
+                        VL2,
+                        CASE
+                    WHEN ISNUMERIC(VL2)=1
+                    THEN
+                    CASE
+                    WHEN CAST(Replace(VL2,',','')AS FLOAT) <=50.90 THEN '<1000 Copies'
+                    WHEN CAST(Replace(VL2,',','') AS FLOAT) between 51.00 and 399.00 THEN '<1000 Copies'
+                    WHEN CAST(Replace(VL2,',','')AS FLOAT) between 400.00 and 999.00 THEN '<1000 Copies'
+                    WHEN CAST(Replace(VL2,',','')AS FLOAT) >=1000 THEN '>1000 Copies'
+                    END
+                    WHEN VL2 IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<1000 Copies'
+                    ELSE NULL END AS [VL2Result]`,
+            ])
+            .leftJoin(
+                'FACT_VLs',
+                'c',
+                'cohort.Patientid= c.PatientID AND cohort.PatientPK= c.PatientPK  AND cohort.MFLCode= c.SiteCode ',
+            );
 
-        // if (query.county) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.County IN (:...counties)', {counties: query.county});
-        // }
-        //
-        // if (query.subCounty) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.SubCounty IN (:...subCounties)', {subCounties: query.subCounty});
-        // }
-        //
-        // if (query.facility) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.FacilityName IN (:...facilities)', {facilities: query.facility});
-        // }
-        //
-        // if (query.partner) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.CTPartner IN (:...partners)', {partners: query.partner});
-        // }
-        //
-        // if (query.agency) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.CTAgency IN (:...agencies)', {agencies: query.agency});
-        // }
-        //
-        // if (query.datimAgeGroup) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.AgeGroup IN (:...ageGroups)', {ageGroups: query.datimAgeGroup});
-        // }
-        //
-        // if (query.gender) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.Gender IN (:...genders)', {genders: query.gender});
-        // }
-        return await this.repository.query(vlOverallNumberFollowSecondlineRegiment);
+        if (query.county) {
+            vlOverallNumberFollowSecondlineRegiment.andWhere('cohort.County IN (:...counties)', {counties: query.county});
+        }
+        
+        if (query.subCounty) {
+            vlOverallNumberFollowSecondlineRegiment.andWhere(
+                'cohort.SubCounty IN (:...subCounties)',
+                { subCounties: query.subCounty },
+            );
+        }
+        
+        if (query.facility) {
+            vlOverallNumberFollowSecondlineRegiment.andWhere(
+                'cohort.FacilityName IN (:...facilities)',
+                { facilities: query.facility },
+            );
+        }
+        
+        if (query.partner) {
+            vlOverallNumberFollowSecondlineRegiment.andWhere(
+                'cohort.CTPartner IN (:...partners)',
+                { partners: query.partner },
+            );
+        }
+        
+        if (query.agency) {
+            vlOverallNumberFollowSecondlineRegiment.andWhere(
+                'cohort.CTAgency IN (:...agencies)',
+                { agencies: query.agency },
+            );
+        }
+        
+        if (query.datimAgeGroup) {
+            vlOverallNumberFollowSecondlineRegiment.andWhere(
+                'cohort.AgeGroup IN (:...ageGroups)',
+                { ageGroups: query.datimAgeGroup },
+            );
+        }
+        
+        if (query.gender) {
+            vlOverallNumberFollowSecondlineRegiment.andWhere(
+                'cohort.Gender IN (:...genders)',
+                { genders: query.gender },
+            );
+        }
+        
+        const originalQuery = vlOverallNumberFollowSecondlineRegiment.getQuery;
+        const originalParams = vlOverallNumberFollowSecondlineRegiment.getParameters;
+        vlOverallNumberFollowSecondlineRegiment.getQuery = () => {
+            const a = originalQuery.call(
+                vlOverallNumberFollowSecondlineRegiment,
+            );
+            return `WITH VL AS (${a}) SELECT VL2Result, Count (*) Num FROM VL WHERE ARTOutcome='V' and  VL2Result   in ('>1000 Copies')  and DATEDIFF(MONTH,VL2Date,GETDATE())<= 14 and currentARTline='Second Line' Group by VL2Result`;
+        };
+
+        vlOverallNumberFollowSecondlineRegiment.getParameters = () => {
+            return originalParams.call(vlOverallNumberFollowSecondlineRegiment);
+        };
+
+        return vlOverallNumberFollowSecondlineRegiment.getRawMany();
     }
 }
--- src/care-treatment/viral-load/queries/handlers/get-vl-overall-uptake-received-follow-tests.handler.ts
+++ src/care-treatment/viral-load/queries/handlers/get-vl-overall-uptake-received-follow-tests.handler.ts
@@ -18,38 +18,101 @@ export class GetVlOverallUptakeReceivedFollowTestsHandler implements IQueryHandl
         //     .where('f.MFLCode > 0')
         //     .andWhere('Last12MVLResult IS NOT NULL');
 
-        const vlOverallUptakeReceivedFollow = "With VL AS (Select MFLCode, uniqueidno_orig as PatientID, faccounty as County, facSubcounty as SubCounty, DOB,\tSex,\tlastVL1 as LastVL,\tdtlastVL2,\tdtlastVL1, ARTOutcome,\tCASE WHEN ISNUMERIC(lastVL1)=1 THEN\n" +
-            "CASE WHEN CAST(Replace(lastVL1,',','')AS FLOAT) <=50.90 THEN '<1000 Copies' WHEN CAST(Replace(lastVL1,',','') AS FLOAT) between 51.00 and 399.00 THEN '<1000 Copies' WHEN CAST(Replace(lastVL1,',','')AS FLOAT) between 400.00 and 999.00 THEN '<1000 Copies' WHEN CAST(Replace(lastVL1,',','')AS FLOAT) >=1000 THEN '>1000 Copies' END WHEN lastVL1 IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<1000 Copies'\n" +
-            "ELSE NULL END AS [LastVLResult],\tdtlastVL1 as DateLAstVL, lastVl2,CASE WHEN ISNUMERIC(lastVL2)=1 THEN CASE WHEN CAST(Replace(lastVL2,',','')AS FLOAT) <=50.90 THEN '<1000 Copies' WHEN CAST(Replace(lastVL2,',','') AS FLOAT) between 51.00 and 399.00 THEN '<1000 Copies' WHEN CAST(Replace(lastVL2,',','')AS FLOAT) between 400.00 and 999.00 THEN '<1000 Copies' WHEN CAST(Replace(lastVL2,',','')AS FLOAT) >=1000 THEN '>1000 Copies' END WHEN lastVL2 IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<1000 Copies' ELSE NULL END AS [LastVL2Result] from All_Staging_2016_2.dbo.Cohort2015_2016 c)\n" +
-            "Select LastVLResult, Count (*) Num from VL where ARTOutcome='V' and  LastVL2Result   in ('>1000 Copies')  and DATEDIFF(MONTH,dtlastVL2,GETDATE())<= 14 group by LastVLResult"
+        const vlOverallUptakeReceivedFollow = this.repository
+            .createQueryBuilder('cohort')
+            .select([
+                `MFLCode, cohort.PatientID,County,SubCounty,DOB,Gender,VL1 as LastVL,VL2Date,VL1Date,ARTOutcome,
+                    CASE
+                        WHEN ISNUMERIC(VL1)=1
+                            THEN
+                        CASE
+                        WHEN CAST(Replace(VL1,',','')AS FLOAT) <=50.90 THEN '<1000 Copies'
+                        WHEN CAST(Replace(VL1,',','') AS FLOAT) between 51.00 and 399.00 THEN '<1000 Copies'
+                        WHEN CAST(Replace(VL1,',','')AS FLOAT) between 400.00 and 999.00 THEN '<1000 Copies'
+                        WHEN CAST(Replace(VL1,',','')AS FLOAT) >=1000 THEN '>1000 Copies'
+                        END
+                        WHEN VL1 IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<1000 Copies'
+                        ELSE NULL END AS [LastVLResult],
+                                VL1Date as DateLAstVL,
+                                VL2,
+                                CASE
+                        WHEN ISNUMERIC(VL2)=1
+                        THEN
+                        CASE
+                        WHEN CAST(Replace(VL2,',','')AS FLOAT) <=50.90 THEN '<1000 Copies'
+                        WHEN CAST(Replace(VL2,',','') AS FLOAT) between 51.00 and 399.00 THEN '<1000 Copies'
+                        WHEN CAST(Replace(VL2,',','')AS FLOAT) between 400.00 and 999.00 THEN '<1000 Copies'
+                        WHEN CAST(Replace(VL2,',','')AS FLOAT) >=1000 THEN '>1000 Copies'
+                        END
+                        WHEN VL2 IN ('undetectable','NOT DETECTED','0 copies/ml','LDL','ND','Target Not Detected',' Not detected','Target Not Detected.','Less than Low Detectable Level') THEN '<1000 Copies'
+                        ELSE NULL END AS [VL2Result]`,
+            ])
+            .leftJoin(
+                'FACT_VLs',
+                'c',
+                'cohort.Patientid=c.PatientID and cohort.PatientPK=c.PatientPK and cohort.MFLCode=c.SiteCode',
+            );
 
-        // if (query.county) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.County IN (:...counties)', {counties: query.county});
-        // }
-        //
-        // if (query.subCounty) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.SubCounty IN (:...subCounties)', {subCounties: query.subCounty});
-        // }
-        //
-        // if (query.facility) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.FacilityName IN (:...facilities)', {facilities: query.facility});
-        // }
-        //
-        // if (query.partner) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.CTPartner IN (:...partners)', {partners: query.partner});
-        // }
-        //
-        // if (query.agency) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.CTAgency IN (:...agencies)', {agencies: query.agency});
-        // }
-        //
-        // if (query.datimAgeGroup) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.AgeGroup IN (:...ageGroups)', {ageGroups: query.datimAgeGroup});
-        // }
-        //
-        // if (query.gender) {
-        //     vlOverallUptakeAndSuppressionBySex.andWhere('f.Gender IN (:...genders)', {genders: query.gender});
-        // }
-        return await this.repository.query(vlOverallUptakeReceivedFollow);
+        if (query.county) {
+            vlOverallUptakeReceivedFollow.andWhere(
+                'cohort.County IN (:...counties)',
+                { counties: query.county },
+            );
+        }
+        
+        if (query.subCounty) {
+            vlOverallUptakeReceivedFollow.andWhere(
+                'cohort.SubCounty IN (:...subCounties)',
+                { subCounties: query.subCounty },
+            );
+        }
+        
+        if (query.facility) {
+            vlOverallUptakeReceivedFollow.andWhere(
+                'cohort.FacilityName IN (:...facilities)',
+                { facilities: query.facility },
+            );
+        }
+        
+        if (query.partner) {
+            vlOverallUptakeReceivedFollow.andWhere(
+                'cohort.CTPartner IN (:...partners)',
+                { partners: query.partner },
+            );
+        }
+        
+        if (query.agency) {
+            vlOverallUptakeReceivedFollow.andWhere(
+                'cohort.CTAgency IN (:...agencies)',
+                { agencies: query.agency },
+            );
+        }
+        
+        if (query.datimAgeGroup) {
+            vlOverallUptakeReceivedFollow.andWhere(
+                'cohort.AgeGroup IN (:...ageGroups)',
+                { ageGroups: query.datimAgeGroup },
+            );
+        }
+        
+        if (query.gender) {
+            vlOverallUptakeReceivedFollow.andWhere(
+                'cohort.Gender IN (:...genders)',
+                { genders: query.gender },
+            );
+        }
+        
+        const originalQuery = vlOverallUptakeReceivedFollow.getQuery;
+        const originalParams = vlOverallUptakeReceivedFollow.getParameters;
+        vlOverallUptakeReceivedFollow.getQuery = () => {
+            const a = originalQuery.call(vlOverallUptakeReceivedFollow);
+            return `WITH VL AS (${a}) SELECT LastVLResult, Count (*) Num FROM VL WHERE ARTOutcome='V' and VL2Result in ('>1000 Copies') and DATEDIFF(MONTH,VL2Date,GETDATE())<= 14 group by LastVLResult`;
+        };
+
+        vlOverallUptakeReceivedFollow.getParameters = () => {
+            return originalParams.call(vlOverallUptakeReceivedFollow);
+        };
+
+        return vlOverallUptakeReceivedFollow.getRawMany();
     }
 }
