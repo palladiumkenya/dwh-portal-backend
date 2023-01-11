5de24e734506e7ee10b9fd2114d4c1e11b88f19a
cbrianbet <cbrianbet@outlook.com>
2023-01-11 23:22:36 +0300

    art verification apis

---
+++ src/care-treatment/art-verification/queries/handlers/get-art-verification-reasons.handler.ts
@@ -0,0 +1,46 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { FactTransNewCohort } from '../../../new-on-art/entities/fact-trans-new-cohort.model';
+import { GetArtVerificationReasonsQuery } from './../impl/get-art-verification-reasons.query';
+
+@QueryHandler(GetArtVerificationReasonsQuery)
+export class GetArtVerificationReasonsHandler
+    implements IQueryHandler<GetArtVerificationReasonsQuery> {
+    constructor(
+        @InjectRepository(FactTransNewCohort, 'mssql')
+        private readonly repository: Repository<FactTransNewCohort>,
+    ) {}
+
+    async execute(query: GetArtVerificationReasonsQuery): Promise<any> {
+        let params = [];
+        const nonReasons = `
+            Select
+                count(*), non_verification_reason
+                FROM [pSurvey].[dbo].[stg_questionnaire_responses]
+				group by non_verification_reason
+        `;
+
+        if (query.county) {
+            params.push(query.county);
+        }
+
+        if (query.subCounty) {
+            params.push(query.subCounty);
+        }
+
+        if (query.facility) {
+            params.push(query.facility);
+        }
+
+        if (query.partner) {
+            params.push(query.partner);
+        }
+
+        if (query.agency) {
+            params.push(query.agency);
+        }
+
+        return await this.repository.query(nonReasons, params);
+    }
+}
