d66c27155346e289fd2138de4e4ec709c0cea559
cbrianbet <cbrianbet@outlook.com>
2023-01-12 12:51:25 +0300

    Update get-art-verification-reasons.handler.ts

--- src/care-treatment/art-verification/queries/handlers/get-art-verification-reasons.handler.ts
+++ src/care-treatment/art-verification/queries/handlers/get-art-verification-reasons.handler.ts
@@ -15,10 +15,47 @@ export class GetArtVerificationReasonsHandler
     async execute(query: GetArtVerificationReasonsQuery): Promise<any> {
         let params = [];
         const nonReasons = `
-            Select
-                count(*) NUM, non_verification_reason
+            with EnrichedFullfacilitylist As (
+                Select
+                    distinct cast(Allsites.MFLCode collate Latin1_General_CI_AS as nvarchar) as MFLCode,
+                    Allsites.FacilityName,
+                    Allsites.County,
+                    Allsites.FacilityType,
+                    EMRs.EMR,
+                    coalesce(EMRs.SDP, Allsites.SDIP) As SDIP,
+                    coalesce(EMRs.[SDP Agency], Allsites.Agency) as Agency
+                from  HIS_Implementation.dbo.EMRandNonEMRSites as Allsites
+                left join HIS_Implementation.dbo.All_EMRSites  EMRs on EMRs.MFL_Code=Allsites.MFLCode
+            ),PSurveys as (
+                Select
+                    *
                 FROM [pSurvey].[dbo].[stg_questionnaire_responses]
-				group by non_verification_reason
+            ), FacilitySummary AS (
+                select
+                    EnrichedFullfacilitylist.MFLCode as MFLCode,
+                    EnrichedFullfacilitylist.FacilityName as Facility,
+                    EnrichedFullfacilitylist.SDIP,
+                    EnrichedFullfacilitylist.EMR as EMR,
+                    EnrichedFullfacilitylist.FacilityType,
+                    EnrichedFullfacilitylist.County,
+                    EnrichedFullfacilitylist.Agency,
+                    PSurveys.non_verification_reason
+                from EnrichedFullfacilitylist
+                left join PSurveys on PSurveys.mfl_code=EnrichedFullfacilitylist.MFLCode collate Latin1_General_CI_AS
+                group by
+                    EnrichedFullfacilitylist.MFLCode,
+                    EnrichedFullfacilitylist.FacilityName,
+                    EnrichedFullfacilitylist.SDIP,
+                    EnrichedFullfacilitylist.EMR,
+                    EnrichedFullfacilitylist.FacilityType,
+                    EnrichedFullfacilitylist.County,
+                    EnrichedFullfacilitylist.Agency,
+                    PSurveys.non_verification_reason
+            ) Select
+                count(*), non_verification_reason
+                FROM FacilitySummary
+                where FacilitySummary.FacilityType = 'emr'
+                group by non_verification_reason
                 order by count(*);
         `;
 
