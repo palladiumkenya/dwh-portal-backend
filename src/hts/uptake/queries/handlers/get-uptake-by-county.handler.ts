53d76c8eb8391c5f1be0ddbca4482f08603ebffb
cbrianbet <cbrianbet@outlook.com>
2023-02-21 19:04:06 +0300

    Update get-uptake-by-county.handler.ts

--- src/hts/uptake/queries/handlers/get-uptake-by-county.handler.ts
+++ src/hts/uptake/queries/handlers/get-uptake-by-county.handler.ts
@@ -85,7 +85,7 @@ export class GetUptakeByCountyHandler
         }
 
         if (query.toDate) {
-            uptakeByCountySql = `${uptakeByCountySql} and DateTestedKey <= EOMONTH('EOMONTH('${query.toDate}01')')`;
+            uptakeByCountySql = `${uptakeByCountySql} and DateTestedKey <= EOMONTH('${query.toDate}01')`;
         }
 
         if (query.county) {
