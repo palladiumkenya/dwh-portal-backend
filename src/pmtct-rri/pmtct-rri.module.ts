e7fbbfc7c6c52fc173eb4525f362e67542d283a6
cbrianbet <cbrianbet@outlook.com>
2023-04-18 13:41:22 +0300

    maternal haart apis added

--- src/pmtct-rri/pmtct-rri.module.ts
+++ src/pmtct-rri/pmtct-rri.module.ts
@@ -7,18 +7,23 @@ import {PmtctRRIController} from './pmtct-rri.controller';
 
 import { AggregateTXCurr } from 'src/care-treatment/current-on-art/entities/aggregate-txcurr.model';
 import { MissedTestingFirstANC } from './missed-anc/entities/missed-testing-first-anc.model';
+import { MissedMaternalHaart } from './missed-haart/entities/missed-maternal-haart.model';
 
 import { GetMissedANCOverviewHandler } from './missed-anc/queries/handlers/get-missed-anc-overview.handler';
 import { GetMissedANCByCountyHandler } from './missed-anc/queries/handlers/get-missed-anc-by-county.handler';
 import { GetMissedANCBySDPHandler } from './missed-anc/queries/handlers/get-missed-anc-by-sdp.handler';
 import { GetMissedANCGapsHandler } from './missed-anc/queries/handlers/get-missed-anc-gaps.handler';
+import { GetMissedHAARTOverviewHandler } from './missed-haart/queries/handlers/get-missed-haart-overview.handler';
+import { GetMissedHAARTBySDPHandler } from './missed-haart/queries/handlers/get-missed-haart-by-sdp.handler';
+import { GetMissedHAARTByCountyHandler } from './missed-haart/queries/handlers/get-missed-haart-by-county.handler';
+import { GetMissedHAARTByFacilityHandler } from './missed-haart/queries/handlers/get-missed-haart-by-facility.handler';
 
 @Module({
     imports: [
         CqrsModule,
         ConfigurationModule,
         TypeOrmModule.forFeature(
-            [AggregateTXCurr, MissedTestingFirstANC],
+            [AggregateTXCurr, MissedTestingFirstANC, MissedMaternalHaart],
             'mssql',
         ),
     ],
@@ -27,6 +32,11 @@ import { GetMissedANCGapsHandler } from './missed-anc/queries/handlers/get-misse
         GetMissedANCByCountyHandler,
         GetMissedANCBySDPHandler,
         GetMissedANCGapsHandler,
+
+        GetMissedHAARTOverviewHandler,
+        GetMissedHAARTBySDPHandler,
+        GetMissedHAARTByCountyHandler,
+        GetMissedHAARTByFacilityHandler,
     ],
     controllers: [PmtctRRIController],
 })
