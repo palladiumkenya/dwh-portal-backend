5f2c08754d60ae2028c369b4ae1d12ff8f866817
cbrianbet <cbrianbet@outlook.com>
2023-04-28 17:46:42 +0300

    moved prep apis to aggregate

--- src/hts/prep/queries/handlers/get-prep-sti-Treatment-outcome.handler.ts
+++ src/hts/prep/queries/handlers/get-prep-sti-Treatment-outcome.handler.ts
@@ -16,18 +16,10 @@ export class GetPrepSTITreatmentOutcomeHandler
     async execute(query: GetPrepSTITreatmentOutcomeQuery): Promise<any> {
         const params = [];
         let newOnPrep = `SELECT
-                sum(case  when [STITreated] = 'Yes' then 1 else 0 END) as Treated,
-                sum(case  when [STITreated] = 'No' then 1 else 0 END) as NotTreated
-            from NDWH.dbo.FactPrep prep
-
-            LEFT JOIN NDWH.dbo.DimPatient pat ON prep.PatientKey = pat.PatientKey
-            LEFT JOIN NDWH.dbo.DimFacility fac ON fac.FacilityKey = prep.FacilityKey
-            LEFT JOIN NDWH.dbo.DimPartner p ON p.PartnerKey = prep.PartnerKey
-            LEFT JOIN NDWH.dbo.DimAgency a ON a.AgencyKey = prep.AgencyKey
-            LEFT JOIN NDWH.dbo.DimAgeGroup age ON age.AgeGroupKey = prep.AgeGroupKey
-            LEFT JOIN NDWH.dbo.DimDate visit ON visit.DateKey = prep.VisitDateKey COLLATE Latin1_General_CI_AS
-            
-            WHERE visit.Date is not null
+                sum(NumberSTITreated) as Treated,
+                sum(NumberSTINotTreated) as NotTreated
+            from AggregatePrepSTIOutcomes prep
+            WHERE MFLCode is not null
         `;
 
         if (query.county) {
@@ -67,17 +59,17 @@ export class GetPrepSTITreatmentOutcomeHandler
         }
 
         if (query.datimAgeGroup) {
-            newOnPrep = `${newOnPrep} and DATIMAgeGroup IN ('${query.datimAgeGroup
+            newOnPrep = `${newOnPrep} and AgeGroup IN ('${query.datimAgeGroup
                 .toString()
                 .replace(/,/g, "','")}')`;
         }
 
         if (query.year) {
-            newOnPrep = `${newOnPrep} and visit.year = ${query.year}`;
+            newOnPrep = `${newOnPrep} and year = ${query.year}`;
         }
 
         if (query.month) {
-            newOnPrep = `${newOnPrep} and visit.month = ${query.month}`;
+            newOnPrep = `${newOnPrep} and month = ${query.month}`;
         }
 
         return await this.repository.query(newOnPrep, params);
