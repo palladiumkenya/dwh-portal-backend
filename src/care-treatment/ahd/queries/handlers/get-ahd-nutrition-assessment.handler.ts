2ae8085828d84d2772327d90be9af1200278e834
cbrianbet <cbrianbet@outlook.com>
2025-02-27 16:34:10 +0300

    bug fix/ ahd filters
    - fixed bug on ahd filters not working

--- src/care-treatment/ahd/queries/handlers/get-ahd-nutrition-assessment.handler.ts
+++ src/care-treatment/ahd/queries/handlers/get-ahd-nutrition-assessment.handler.ts
@@ -12,6 +12,37 @@ export class GetAhdNutritionAssessmentHandler implements IQueryHandler<GetAhdNut
     ) {}
 
     async execute(query: GetAhdNutritionAssessmentQuery): Promise<any> {
+        const filters = [];
+        if (query.county) {
+            filters.push('County In (:...counties)')
+        }
+
+        if (query.subCounty) {
+            filters.push('SubCounty In (:...subCounties)')
+        }
+
+        if (query.facility) {
+            filters.push('FacilityName In (:...facilities)')
+        }
+
+        if (query.partner) {
+            filters.push('PartnerName In (:...partners)')
+        }
+
+        if (query.agency) {
+            filters.push('AgencyName In (:...agencies)')
+        }
+
+        if (query.datimAgeGroup) {
+            filters.push('AgeGroup In (:...ageGroups)')
+        }
+
+        if (query.gender) {
+            filters.push('Gender In (:...genders)')
+        }
+
+        const whereClause = filters.length ? `WHERE ${filters.join(' AND ')}` : ``;
+
         const ahdAssessment = this.repository
             .createQueryBuilder('f')
             .select([`TOP 1
@@ -20,69 +51,41 @@ export class GetAhdNutritionAssessmentHandler implements IQueryHandler<GetAhdNut
                   SUM(CASE WHEN ConfirmedTreatmentFailure = 1 THEN 1 ELSE 0 END) +
                   SUM(CASE WHEN NewPatient = 1 THEN 1 ELSE 0 END) AS TotalCount
                 FROM REPORTING.dbo.linelist_FactART
-                WHERE AHD=1 and age <15) AS ChildrenWithAHD,
+                    ${whereClause} ${filters.length ? ' AND ': 'WHERE '}
+                     AHD=1 and age <15) AS ChildrenWithAHD,
                 (SELECT 
                   SUM(CASE WHEN IsRTTLast12MonthsAfter3monthsIIT = 1 THEN 1 ELSE 0 END) +
                   SUM(CASE WHEN ConfirmedTreatmentFailure = 1 THEN 1 ELSE 0 END) +
                   SUM(CASE WHEN NewPatient = 1 THEN 1 ELSE 0 END) AS TotalCount
                 FROM REPORTING.dbo.linelist_FactART
-                WHERE AHD=1 and age <15 and ScreenedForMalnutrition=1) AS ScreenedForMalnutrition,
+                    ${whereClause} ${filters.length ? ' AND ': 'WHERE '}
+                 AHD=1 and age <15 and ScreenedForMalnutrition=1) AS ScreenedForMalnutrition,
                 (SELECT 
                   SUM(CASE WHEN IsRTTLast12MonthsAfter3monthsIIT = 1 THEN 1 ELSE 0 END) +
                   SUM(CASE WHEN ConfirmedTreatmentFailure = 1 THEN 1 ELSE 0 END) +
                   SUM(CASE WHEN NewPatient = 1 THEN 1 ELSE 0 END) AS TotalCount
                 FROM REPORTING.dbo.linelist_FactART
-                WHERE AHD=1 and age <15 and SAM=1) AS NumberWithSAM,
+                    ${whereClause} ${filters.length ? ' AND ': 'WHERE '}
+                 AHD=1 and age <15 and SAM=1) AS NumberWithSAM,
                 (SELECT 
                   SUM(CASE WHEN IsRTTLast12MonthsAfter3monthsIIT = 1 THEN 1 ELSE 0 END) +
                   SUM(CASE WHEN ConfirmedTreatmentFailure = 1 THEN 1 ELSE 0 END) +
                   SUM(CASE WHEN NewPatient = 1 THEN 1 ELSE 0 END) AS TotalCount
                 FROM REPORTING.dbo.linelist_FactART
-                WHERE AHD=1 and age <15 and MAM=1) AS NumberWithMAM
+                    ${whereClause} ${filters.length ? ' AND ': 'WHERE '}
+                 AHD=1 and age <15 and MAM=1) AS NumberWithMAM
             `])
 
-        if (query.county) {
-            ahdAssessment.andWhere('f.County IN (:...counties)', {
-                counties: query.county,
-            });
+        const params = {
+            facilities: query.facility,
+            counties: query.county,
+            subCounties: query.subCounty,
+            partners: query.partner,
+            agencies: query.agency,
+            ageGroups: query.datimAgeGroup,
+            genders: query.gender
         }
 
-        if (query.subCounty) {
-            ahdAssessment.andWhere('f.Subcounty IN (:...subCounties)', {
-                subCounties: query.subCounty,
-            });
-        }
-
-        if (query.facility) {
-            ahdAssessment.andWhere('f.FacilityName IN (:...facilities)', {
-                facilities: query.facility,
-            });
-        }
-
-        if (query.partner) {
-            ahdAssessment.andWhere('f.PartnerName IN (:...partners)', {
-                partners: query.partner,
-            });
-        }
-
-        if (query.agency) {
-            ahdAssessment.andWhere('f.AgencyName IN (:...agencies)', {
-                agencies: query.agency,
-            });
-        }
-
-        if (query.datimAgeGroup) {
-            ahdAssessment.andWhere('f.AgeGroup IN (:...ageGroups)', {
-                ageGroups: query.datimAgeGroup,
-            });
-        }
-
-        if (query.gender) {
-            ahdAssessment.andWhere('f.Gender IN (:...genders)', {
-                genders: query.gender,
-            });
-        }
-
-        return await ahdAssessment.getRawOne();
+        return await ahdAssessment.setParameters(params).getRawOne();
     }
 }
