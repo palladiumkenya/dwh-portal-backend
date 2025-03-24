f96701fd5ea7f8b632eb2704a45bf7e815836b32
cbrianbet <cbrianbet@outlook.com>
2025-03-24 13:00:06 +0300

    Update get-ct-tx-curr-age-group-distribution-by-partner.handler.ts

--- src/care-treatment/current-on-art/queries/handlers/get-ct-tx-curr-age-group-distribution-by-partner.handler.ts
+++ src/care-treatment/current-on-art/queries/handlers/get-ct-tx-curr-age-group-distribution-by-partner.handler.ts
@@ -3,9 +3,8 @@ import {
     GetCtTxCurrAgeGroupDistributionByPartnerQuery
 } from '../impl/get-ct-tx-curr-age-group-distribution-by-partner.query';
 import {InjectRepository} from '@nestjs/typeorm';
-import {FactTransHmisStatsTxcurr} from '../../entities/fact-trans-hmis-stats-txcurr.model';
 import {Repository} from 'typeorm';
-import { AggregateTXCurr } from './../../entities/aggregate-txcurr.model';
+import { AggregateTXCurr } from '../../entities/aggregate-txcurr.model';
 
 @QueryHandler(GetCtTxCurrAgeGroupDistributionByPartnerQuery)
 export class GetCtTxCurrAgeGroupDistributionByPartnerHandler implements IQueryHandler<GetCtTxCurrAgeGroupDistributionByPartnerQuery> {
@@ -19,7 +18,7 @@ export class GetCtTxCurrAgeGroupDistributionByPartnerHandler implements IQueryHa
         const txCurrAgeGroupDistributionByPartner = this.repository
             .createQueryBuilder('f')
             .select([
-                '[PartnerName] CTPartner, f.[DATIMAgeGroup] ageGroup, Gender, SUM([CountClientsTXCur]) txCurr',
+                '[PartnerName] CTPartner, f.[DATIMAgeGroup] ageGroup, Sex Gender, SUM([CountClientsTXCur]) txCurr',
             ])
             // .innerJoin(DimAgeGroups, 'v', 'f.ageGroup = v.AgeGroup')
             .where(
@@ -53,7 +52,7 @@ export class GetCtTxCurrAgeGroupDistributionByPartnerHandler implements IQueryHa
 
         if (query.gender) {
             txCurrAgeGroupDistributionByPartner
-                .andWhere('f.Gender IN (:...genders)', { genders: query.gender });
+                .andWhere('f.Sex IN (:...genders)', { genders: query.gender });
         }
 
         if (query.datimAgeGroup) {
@@ -64,7 +63,7 @@ export class GetCtTxCurrAgeGroupDistributionByPartnerHandler implements IQueryHa
         }
 
         return await txCurrAgeGroupDistributionByPartner
-            .groupBy('[PartnerName], f.[DATIMAgeGroup], Gender')
+            .groupBy('[PartnerName], f.[DATIMAgeGroup], Sex')
             .orderBy('SUM([CountClientsTXCur])', 'DESC')
             .getRawMany();
     }
